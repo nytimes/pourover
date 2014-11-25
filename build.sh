c1ba0b7027413963e52406d8f3d8529cd51d9b72
Erik Hinton <erik.hinton@nytimes.com>
2014-11-25 14:41:14 -0500

    JEKYLL BUILD

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 git pull --commit origin gh-pages
 git checkout master examples
 jekyll build
+docco _site/examples/*.js -o _site/examples/examples_build
 git commit -am "JEKYLL BUILD"
 git push origin gh-pages
