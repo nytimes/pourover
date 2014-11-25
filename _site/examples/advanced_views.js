87118478727ea458e8785e3a2b4ea833e0cb211d
Erik Hinton <erik.hinton@nytimes.com>
2014-11-25 14:37:56 -0500

    JEKYLL BUILD

--- _site/examples/advanced_views.js
+++ _site/examples/advanced_views.js
@@ -34,7 +34,8 @@ var view = new PourOver.View("default_view", collection);
 // #### Creating a custom sort
 // To create a custom sort, we generally extend the Sort object with a `fn`, the comparator function used to order items.
 // We also specify which attribute the sort operates over -- what it sorts with respect to --  with the `attr` attribute.
-// Then, we instantiate our new sort.
+// Then, we instantiate our new sort. This attr field has no special signifigance other than convention. You must still
+// manually extract values in your sort function.
 var RevNameSort = PourOver.Sort.extend({
     fn: function(a,b){
         var x = a.name, y = b.name;    
